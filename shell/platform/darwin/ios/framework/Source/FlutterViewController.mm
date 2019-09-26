189e47ad57f986c063dc1c2766fc13b9f1cbfe64
junzai.yxj <junzai.yxj@alibaba-inc.com>
2019-09-26 14:38:39 +0800

    [ICBU] shared engine surfaceUpdate state; navbar height;

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -477,6 +477,14 @@ typedef enum UIAccessibilityContrast : NSInteger {
   [self onAccessibilityStatusChanged:nil];
   [[_engine.get() lifecycleChannel] sendMessage:@"AppLifecycleState.resumed"];
 
+    if ([_engine.get() viewController] != self) {
+        if (_viewportMetrics.physical_width)
+            [self surfaceUpdated:YES];
+        [_engine.get() setViewController:self];
+        if (_viewportMetrics.physical_width)
+            [self surfaceUpdated:YES];
+    }
+    
   [super viewDidAppear:animated];
 }
 
@@ -489,8 +497,13 @@ typedef enum UIAccessibilityContrast : NSInteger {
 
 - (void)viewDidDisappear:(BOOL)animated {
   TRACE_EVENT0("flutter", "viewDidDisappear");
-  [self surfaceUpdated:NO];
-  [[_engine.get() lifecycleChannel] sendMessage:@"AppLifecycleState.paused"];
+//  [self surfaceUpdated:NO];
+//  [[_engine.get() lifecycleChannel] sendMessage:@"AppLifecycleState.paused"];
+    if ([_engine.get() viewController] == self) {
+        [self surfaceUpdated:NO];
+        [[_engine.get() lifecycleChannel] sendMessage:@"AppLifecycleState.paused"];
+    }
+    
   [self flushOngoingTouches];
 
   [super viewDidDisappear:animated];
@@ -760,7 +773,8 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
   _viewportMetrics.physical_width = viewSize.width * scale;
   _viewportMetrics.physical_height = viewSize.height * scale;
 
-  [self updateViewportPadding];
+//  [self updateViewportPadding];
+    [self updateViewportPadding:NO];
   [self updateViewportMetrics];
 
   // This must run after updateViewportMetrics so that the surface creation tasks are queued after
@@ -784,7 +798,8 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 }
 
 - (void)viewSafeAreaInsetsDidChange {
-  [self updateViewportPadding];
+//  [self updateViewportPadding];
+    [self updateViewportPadding:YES];
   [self updateViewportMetrics];
   [super viewSafeAreaInsetsDidChange];
 }
@@ -792,16 +807,30 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 // Updates _viewportMetrics physical padding.
 //
 // Viewport padding represents the iOS safe area insets.
-- (void)updateViewportPadding {
-  CGFloat scale = [UIScreen mainScreen].scale;
-  if (@available(iOS 11, *)) {
-    _viewportMetrics.physical_padding_top = self.view.safeAreaInsets.top * scale;
-    _viewportMetrics.physical_padding_left = self.view.safeAreaInsets.left * scale;
-    _viewportMetrics.physical_padding_right = self.view.safeAreaInsets.right * scale;
-    _viewportMetrics.physical_padding_bottom = self.view.safeAreaInsets.bottom * scale;
-  } else {
-    _viewportMetrics.physical_padding_top = [self statusBarPadding] * scale;
-  }
+//- (void)updateViewportPadding {
+//  CGFloat scale = [UIScreen mainScreen].scale;
+//  if (@available(iOS 11, *)) {
+//    _viewportMetrics.physical_padding_top = self.view.safeAreaInsets.top * scale;
+//    _viewportMetrics.physical_padding_left = self.view.safeAreaInsets.left * scale;
+//    _viewportMetrics.physical_padding_right = self.view.safeAreaInsets.right * scale;
+//    _viewportMetrics.physical_padding_bottom = self.view.safeAreaInsets.bottom * scale;
+//  } else {
+//    _viewportMetrics.physical_padding_top = [self statusBarPadding] * scale;
+//  }
+//}
+- (void)updateViewportPadding:(BOOL)filter {
+    CGFloat scale = [UIScreen mainScreen].scale;
+    if (@available(iOS 11, *)) {
+        // 解决从后台进入前台导致错误下移 横屏可能会有问题
+        if (!filter) {
+            _viewportMetrics.physical_padding_top = self.flutterView.safeAreaInsets.top * scale;
+            _viewportMetrics.physical_padding_bottom = self.flutterView.safeAreaInsets.bottom * scale;
+        }
+        _viewportMetrics.physical_padding_left = self.flutterView.safeAreaInsets.left * scale;
+        _viewportMetrics.physical_padding_right = self.flutterView.safeAreaInsets.right * scale;
+    } else {
+        _viewportMetrics.physical_padding_top = [self statusBarPadding] * scale;
+    }
 }
 
 #pragma mark - Keyboard events
